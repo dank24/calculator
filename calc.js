949f5305e3bc4241e60af7733fa40ebbf3c3917f
dans <dansdana94@gmail.com>
2024-02-24 15:28:09 +0100

    ui and functions created

--- calc.js
+++ calc.js
@@ -3,17 +3,22 @@ const btns = document.querySelectorAll('.btns')
 const topScreen = document.querySelector('.topScreen')
 const midScreen = document.querySelector('.midScreen')
 const bottomScreen = document.querySelector('.bottomScreen')
+let operBtns = document.querySelectorAll('.oper')
 
 //variables for input
 let v = [];
 let v1 = '';
 let oper = '';
+
 let op1 = '';
 let ope = '';
 let op2 = '';
 
 
 function operation(op1, ope, op2) {
+    op1 = parseFloat(op1)
+    op2 = parseFloat(op2)
+
     switch (ope) {
     case '+' : return op1 + op2 ;
     break;
@@ -33,17 +38,48 @@ function display() {
                 v1 = v.join('')
             } else {
                 oper = e.id
+                ope = oper
+                v = []
             }
             bottomScreen.textContent = v1
-            console.log(v)
-            console.log(v1)
-            console.log(oper)
+            topScreen.textContent = `${op1} ${ope} ${op2}`
         })
     })
 }
-display()
 
 //storeValues for operation
-function storeValue() {
-
+function storeValues() {
+    btns.forEach(e =>{
+        e.addEventListener('click', () =>{
+        
+            if(ope === ''){
+                op1 = v1
+            } else if(ope !== ''){
+                op2 = v1 
+            } 
+            console.log(op1)
+            console.log(ope)
+            console.log(op2)
+        })
+    })
 }
+
+//calculation
+function calculate() {
+
+    storeValues()
+    display()
+    operBtns.forEach(e =>{
+        e.addEventListener('click', () =>{
+            e.style.backgroundColor = 'red'
+            if(op2 && ope && op2) {
+                let results = operation(op1,ope,op2)
+                midScreen.textContent = results
+                topScreen.textContent = `${op1} : ${ope} : ${op2}`
+            }
+        })
+    })
+}
+
+
+calculate()
